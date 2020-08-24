501c15d18ef710567686c5b3fa282912e06eb080
ankit-github123 <ankit190900@gmail.com>
2020-08-24 11:04:27 +0530

    Updates

---
+++ precache-manifest.b158cd0584363234de72717456e05b57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f442b838976146f2efe6a04319bcd8af",
+    "url": "/fun-app/index.html"
+  },
+  {
+    "revision": "5f88035c53ba84c45b47",
+    "url": "/fun-app/static/js/2.cb13befb.chunk.js"
+  },
+  {
+    "revision": "9a526e863ac08eb62d31",
+    "url": "/fun-app/static/js/main.07453027.chunk.js"
+  },
+  {
+    "revision": "d475559106d95cff56f9",
+    "url": "/fun-app/static/js/runtime-main.96d784e9.js"
+  },
+  {
+    "revision": "e3b343be3c1ce6615453cfb668ad625a",
+    "url": "/fun-app/static/media/defaultPic.e3b343be.png"
+  },
+  {
+    "revision": "ef97be54bea0538affbf33a55a3a0c22",
+    "url": "/fun-app/static/media/laughing.ef97be54.gif"
+  },
+  {
+    "revision": "d41f02a46fc2346cbde83b592ebb4aca",
+    "url": "/fun-app/static/media/writer.d41f02a4.jpg"
+  }
+]);
